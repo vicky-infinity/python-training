710b2055d0d9c05666ff56fe607ebb00b65b98c8
Vicky Vijay Patil <vicky.patil@valuemomentum.com>
2026-07-06 12:51:43 +0530

    added day 5 file

---
+++ day_5_python/app.py
@@ -0,0 +1,33 @@
+#Class and object
+# Parent class
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def show_name(self):
+        print(f"My name is {self.name}")
+
+    def bark(self):
+        print("barking barking ")
+
+# Child Class
+class child(Animal):
+    #The veriables that have mentioned into the parent class and required by the parent class that have to receve into the child class constructure and the super.__init__ as well which is parent class construtre 
+    def __init__(self, name, roll):
+        #Calling the parent class constructure the following is the parent class constructure
+        super().__init__(name) 
+        self.roll = roll
+
+    def show_roll(self):
+        result = self.roll
+        return result
+
+
+myob = child("Ron", 88)
+
+
+result = myob.show_roll()
+
+print(result)
+
+
