182d306594d9e5ab83a687fb56340ee04fc154f9
Vicky Vijay Patil <vicky.patil@valuemomentum.com>
2026-07-09 12:46:31 +0530

    rename

---
+++ day_7_python/specificerror.py
@@ -0,0 +1,22 @@
+def divide():
+    try:
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+
+        result = num1 / num2
+        print("Result:", result)
+
+# This will catch the divide by 0 error
+    except ZeroDivisionError:
+        print("Cannot divide by zero")
+
+# This is for invalid input type
+# This is how we can print the error
+    except ValueError as r:
+        #print("Please enter numbers only")
+        print(f"This is the error {r}")
+
+    finally:
+        print("Program completed")
+
+divide()
