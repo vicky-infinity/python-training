cf03be86672277c7d026ea1d9f1bfcea69c3114e
Vicky Vijay Patil <vicky.patil@valuemomentum.com>
2026-07-20 16:37:55 +0530

    minor import and comment

--- day_14_revision_python/lambda.py
+++ day_14_revision_python/lambda.py
@@ -1,9 +1,11 @@
+import random
 # Lamda function 
 
 """A lambda function is a small anonymous function in Python that can have any
  number of arguments but only one expression. It is typically used for short,
    simple operations where defining a full function with def would be unnecessary."""
 
+# Normal funtion and lambda function works the same
 # Syntax
 # lambda arguments: expression
 # lambda a,b: a+b
@@ -11,9 +13,9 @@
 addi = lambda a,b: a+b
 
 print(addi(5,5))
-def apply(fx,b):
-    result = fx+b
+def apply(fx,num):
+    result = fx()*num
     return result
 
-print(apply(lambda a,b,c: a+b+c, 5))
+print(apply(lambda : random.randint(1,50), 5))
 
